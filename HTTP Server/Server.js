ef0513353657db546ddc2be8fb0de8de21d11ba9
Ahmed Shaikh <f201064@cfd.nu.edu.pk>
2024-09-08 16:21:46 +0500

    Server created and listening on port 5500

--- HTTP Server/Server.js
+++ HTTP Server/Server.js
@@ -1,4 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-
+    res.end("Hello server!");
 })
+
+server.listen(5500, "127.0.0.1", () => {
+    console.log("Listening to the port 5500");
+});
