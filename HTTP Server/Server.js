6cf1d6740575169a3f34b2f1be3aefbadc7f40e0
Ahmed Shaikh <f201064@cfd.nu.edu.pk>
2024-09-08 16:54:21 +0500

    Node js routing (Server)

--- HTTP Server/Server.js
+++ HTTP Server/Server.js
@@ -1,6 +1,20 @@
 const http = require("http");
+
 const server = http.createServer((req, res) => {
-    res.end("Hello server!");
+    //console.log(req.url);
+
+    if (req.url == "/"){
+        res.end("Hello server!");
+    }
+    else if(req.url == "/about"){
+        //res.write('hello nigga!')
+        res.end("Hello about server!");
+    }
+    else{
+        res.writeHead(404, {"Contect-type": "text/html"});
+        res.end("404 client error");
+    }
+    
 })
 
 server.listen(5500, "127.0.0.1", () => {
